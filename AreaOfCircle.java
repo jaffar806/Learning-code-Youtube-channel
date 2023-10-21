ba18862f71cc88d0931c4dab63e3c5109e33f1ba
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-10-21 22:40:14 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+package learncodingyouyubechannel;
+
+public class AreaOfCircle {
+	double pi =3.142, area;
+	AreaOfCircle(double r){
+		area =pi*r*r;
+		System.out.println("Area of circle is: "+area);
+	}
+
+}

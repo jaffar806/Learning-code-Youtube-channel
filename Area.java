be783d65c884dc8c03f301ce7e5d5e6052af9969
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-10-21 22:39:39 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,20 @@
+package learncodingyouyubechannel;
+
+import java.util.Scanner;
+
+public class Area  {
+
+	public static void main(String[] args) {
+		 {   
+		      Scanner s= new Scanner(System.in);
+		      System.out.println("Enter the radius:");
+		      double r= s.nextDouble();
+		      
+		      //we created a constructor with a paramerter of r(radius)
+		      AreaOfCircle  a=new AreaOfCircle(r);  //after calculating radius here-->Areasss Class 
+		          
+		   }
+
+	}
+
+}
